bfd6e4683f91ad9b710699926b7bfc4342faf9a8
Jasper Moelker <jasper@voorhoede.nl>
2023-08-08 16:55:29 +0200

    feat: basic i18n setup
    - adds CMS connection with typings
    - adds translation model in CMS (using scripted migration)
    - adds i18n helper for locales & translations in templates
    - adds localised routing
    - adds basic locale switcher

---
+++ src/lib/datocms.ts
@@ -0,0 +1,42 @@
+export const datocmsRequest = ({ query, variables = {}, preview = false }) => {
+  const endpoint = preview
+    ? "https://graphql.datocms.com/preview"
+    : "https://graphql.datocms.com/"
+
+  return fetch(endpoint, {
+    method: "post",
+    headers: {
+      Authorization: import.meta.env.DATOCMS_READONLY_API_TOKEN,
+      "Content-Type": "application/json",
+      "X-Environment": import.meta.env.DATOCMS_ENVIRONMENT,
+      "X-Exclude-Invalid": "true", // https://www.datocms.com/docs/content-delivery-api/api-endpoints#strict-mode-for-non-nullable-graphql-types
+      // "X-Include-Drafts": preview ? "true" : "false",
+    },
+    body: JSON.stringify({ query, variables }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.errors) throw Error(JSON.stringify(response, null, 4))
+      return response.data;
+    })
+}
+
+/**
+ * Returns a DatoCMS subscription
+ * token is only exposed when app is in preview mode
+ */
+export const datocmsSubscription = async ({
+  query,
+  variables = {},
+  preview = false,
+}) => {
+  const initialData = await datocmsRequest({ query, variables, preview })
+  return {
+    query,
+    variables,
+    preview,
+    initialData,
+    enabled: preview,
+    token: preview && process.env.DATOCMS_READONLY_API_TOKEN,
+  }
+}
