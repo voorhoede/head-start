c55268ebe1c210640a320222662462f5a50a403c
decrek <declanrek@gmail.com>
2023-09-22 13:56:48 +0200

    add editorconfig and run on all files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    server: { port: 4323 }, // 4323 is "head" in T9
+	server: { port: 4323 }, // 4323 is "head" in T9
 })
