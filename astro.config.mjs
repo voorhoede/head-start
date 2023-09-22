ddc3c59d27733081a40aeb5519ee457a24b495ce
decrek <declanrek@gmail.com>
2023-09-22 16:02:45 +0200

    use graphql loader to import graphql files, so we can use fragments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import graphql from '@rollup/plugin-graphql';
 
 // https://astro.build/config
 export default defineConfig({
   server: { port: 4323 }, // 4323 is "head" in T9
+  vite: {
+    plugins: [graphql()]
+  }
 })
