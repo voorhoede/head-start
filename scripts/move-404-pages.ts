df718caf63d38d868785ea77d94ea5eb83a90969
decrek <declanrek@gmail.com>
2023-10-23 11:48:59 +0200

    review comments

--- scripts/move-404-pages.ts
+++ scripts/move-404-pages.ts
@@ -4,28 +4,32 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import { writeFile, readFile, unlink } from 'node:fs/promises';
 import { globby } from 'globby';
+import { defaultLocale } from '../src/lib/i18n';
+
 
 /**
  * Rewrite localised "/:locale/404/index.html" to "/:locale/404.html" so Cloudflare picks them up automatically.
+ * Create a default 404 page, i.e. "/404.html" from the default locale.
  * See https://developers.cloudflare.com/pages/platform/serving-pages/#not-found-behavior
  */
 async function move404Pages() {
-  return globby(path.join(__dirname, '../dist/**/404/index.html'))
-    .then(async filepaths => {
-      return filepaths.map(filepath => {
-        return moveFile(filepath, filepath.replace('404/index.html', '404.html'));
-      });
-    });
+  // create fallback 404 page from default locale
+  const defaultLocale404Page = await readFile(path.join(__dirname, `../dist/${defaultLocale}/404/index.html`));
+  await writeFile(path.join(__dirname, '../dist/404.html'), defaultLocale404Page);
+  
+  // rewrite localised 404 pages
+  const filepaths = await globby(path.join(__dirname, '../dist/**/404/index.html'));
+  return filepaths.map(filepath => {
+    return moveFile(filepath, filepath.replace('404/index.html', '404.html'));
+  });
 }
 
 async function moveFile(filepath: string, newFilepath: string ) {
   const content = await readFile(filepath, 'utf8');
 
   // create new file and remove old one
-  return Promise.all([
-    writeFile(newFilepath, content, 'utf8'),
-    unlink(filepath)
-  ]);
+  await writeFile(newFilepath, content, 'utf8');
+  await unlink(filepath);
 }
 
 move404Pages()
