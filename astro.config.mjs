c197085d2cb6c93047d434c79017ca4c4b12b0d9
Jasper Moelker <jasper@voorhoede.nl>
2023-11-06 19:14:40 +0100

    fix: set & use Astro.site as base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import graphql from '@rollup/plugin-graphql';
 
+const localhostPort = 4323; // 4323 is "head" in T9
+
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 4323 }, // 4323 is "head" in T9
+  server: { port: localhostPort },
+  site: process.env.CF_PAGES
+    ? process.env.CF_PAGES_URL
+    : `http://localhost:${localhostPort}`,
+  trailingSlash: "always",
   vite: {
-    plugins: [graphql()]
-  }
+    plugins: [graphql()],
+  },
 });
