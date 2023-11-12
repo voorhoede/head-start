c0771ecd12c37d289f03698bbeffaef7f2f918eb
Jasper Moelker <jasper@voorhoede.nl>
2023-11-12 19:58:12 +0100

    chore: generate & use i18n SiteLocale and TranslationKey types

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,19 +1,21 @@
+import type { SiteLocale, TranslationKey } from './i18n.types';
 import rosetta from 'rosetta';
 import messages from './i18n.messages.json';
 
-export const locales = ['en', 'nl'];
+export const locales: SiteLocale[] = ['en', 'nl'];
 export const defaultLocale = locales[0];
 
 const i18n = rosetta(messages);
 i18n.locale(defaultLocale);
 
-export const t = i18n.t.bind(i18n);
+export type T = typeof i18n.t & ((key: TranslationKey) => string);
+export const t: T = i18n.t.bind(i18n);
 
 export function getLocale() {
   return i18n.locale();
 }
 
-export function setLocale(locale?: string) {
+export function setLocale(locale?: SiteLocale) {
   const newLocale = locale || defaultLocale;
   return i18n.locale(newLocale);
 }
---
+++ src/lib/i18n.types.ts
@@ -0,0 +1,11 @@
+export type TranslationKey = 
+ | 'image_unavailable'
+ | 'play_video_title'
+ | 'select_language'
+ | 'share_on_social'
+ | 'skip_to_content'
+ | 'not_found'
+ | 'watch_video_on_provider';
+export type SiteLocale = 
+ | 'en'
+ | 'nl';
