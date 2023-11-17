4e2350c2bef85683a0c9e39c41ff326e644c26f1
Jasper Moelker <jasper@voorhoede.nl>
2023-11-17 12:46:16 +0100

    chore: generate & use i18n types (#59)
    # Changes
    
    Adds autocomplete to `t()` function based on available translations in
    DatoCMS. For example `t('play_video')`.
    These suggestions are provided by generating a `TranslationKey` enum
    which is based on the `key`s of translation records in DatoCMS.
    
    # Associated issue
    
    N/A
    
    # How to test
    
    1. Checkout this branch.
    2. Use the `t()` function.
    3. Verify you're getting suggestions based on the `TranslationKey`s from
    the generated `i18n.types.ts` file.
    
    # Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] I have made sure that my PR is easy to review (not too big,
    includes comments)
    - ~~I have added/updated tests to prove that my feature works (if not
    possible please explain why)~~
    - ~~I have made changes to the README and if the change affects the
    project setup (npm commands changed, new service added, environmental
    variable added)~~
    - ~~I have added a decision log entry if the change affects the
    architecture or changes a significant technology~~
    - [x] I have notified a reviewer
    
    <!-- Please strike through and check off all items that do not apply
    (rather than removing them) -->
    
    ---------
    
    Co-authored-by: decrek <declanrek@gmail.com>

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,21 +1,24 @@
+import type { SiteLocale, TranslationKey } from './i18n.types';
 import rosetta from 'rosetta';
 import messages from './i18n.messages.json';
 
-export const locales = ['en', 'nl'];
+export const locales: SiteLocale[] = ['en', 'nl'];
 export const defaultLocale = locales[0];
 export const cookieName = 'HEAD_START_LOCALE';
 
 const i18n = rosetta(messages);
 i18n.locale(defaultLocale);
 
-export const t = i18n.t.bind(i18n);
+export type T = ((key: TranslationKey) => string);
+export const t: T = i18n.t.bind(i18n);
 
 export function getLocale() {
   return i18n.locale();
 }
 
-export function setLocale(locale: string = '') {
-  if (locales.includes(locale)) {
+
+export function setLocale(locale?: SiteLocale) {
+  if (locale && locales.includes(locale)) {
     return i18n.locale(locale);
   }
   return i18n.locale();
