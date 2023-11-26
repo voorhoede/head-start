905a16994a5ce87d408ca519961862d836ce21fc
Jasper Moelker <jasper@voorhoede.nl>
2023-11-26 18:20:26 +0100

    feat(search): link to search in app header

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -7,3 +7,6 @@ export const hasValidQuery = (query: string) => (query.length >= minQueryLength)
 export const getSearchPathname = (locale = getLocale()) => `/${ locale }/search/`;
 export const getOpenSearchName = (locale = getLocale()) => `${globalSeo[locale as keyof typeof globalSeo]?.siteName} (${ locale })`;
 export const getOpenSearchPathname = (locale: string) => `${ getSearchPathname(locale) }opensearch.xml`;
+
+// https://www.datocms.com/docs/site-search/excluding-text
+export const datocmsNoIndex = { 'data-datocms-noindex': '' };
--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -1,9 +1,16 @@
 import type { Tag } from './types/datocms';
 import { getLocale } from './i18n';
 import { globalSeo } from './site.json';
+import type { SiteLocale } from './i18n.types';
+
+export type PageUrl = { 
+  locale: SiteLocale, 
+  pathname: string,
+};
 
 const locale = getLocale();
 
+export const siteName = globalSeo[locale as keyof typeof globalSeo].siteName;
 export const titleSuffix = globalSeo[locale as keyof typeof globalSeo].titleSuffix;
 
 export const noIndexTag: Tag = {
