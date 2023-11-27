dd0898a1493e39ba02aceb6e64cbab52b8b15293
Jasper Moelker <jasper@voorhoede.nl>
2023-11-27 09:15:18 +0100

    feature: XML sitemap and robots.txt (#67)
    # Changes
    
    - Generates XML sitemaps for all pages, based on `getStaticPaths()` and
    using the official
    [`@astro/sitemap`](https://docs.astro.build/en/guides/integrations-guide/sitemap/)
    package.
    - Adds a basic `robots.txt` file.
    - Adds a reference to the generated XML sitemap in the `robots.txt` with
    the deployment's site URL.
    
    # Associated issue
    
    Resolves #5 
    
    # How to test
    
    1. Open preview link
    2. Navigate to `/sitemap-index.xml`
    3. Verify the sitemap is generated and correctly links to sub sitemaps
    4. Navigate to the sub sitemap from the sitemap index (`/sitemap-0.xml`)
    5. Verify it correctly lists all available pages (check that one exists
    for example)
    6. Navigate to `/robots.txt`
    7. Verify it correctly has a rule pointing to the sitemap index
    8. Navigate to any HTML page on the website
    9. Verify that it has a `link[rel="sitemap"]` correctly pointing to the
    sitemap index
    
    # Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] I have made sure that my PR is easy to review (not too big,
    includes comments)
    - ~~I have added/updated tests to prove that my feature works (if not
    possible please explain why)~~
    - ~~I have made changes to the README and if the change affects the
    project setup (npm commands changed, new service added, environmental
    variable added)~~
    - ~~I have added a decision log entry if the change affects the
    architecture or changes a significant technology~~
    - [x] I have notified a reviewer
    
    <!-- Please strike through and check off all items that do not apply
    (rather than removing them) -->

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import graphql from '@rollup/plugin-graphql';
+import sitemap from '@astrojs/sitemap';
 
 const localhostPort = 4323; // 4323 is "head" in T9
+export const siteUrl = process.env.CF_PAGES
+  ? process.env.CF_PAGES_URL
+  : `http://localhost:${localhostPort}`;
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,11 +17,10 @@ export default defineConfig({
       mode: 'local',
     },
   }),
+  integrations: [sitemap()],
   output: 'hybrid',
   server: { port: localhostPort },
-  site: process.env.CF_PAGES
-    ? process.env.CF_PAGES_URL
-    : `http://localhost:${localhostPort}`,
+  site: siteUrl,
   trailingSlash: 'always',
   vite: {
     plugins: [graphql()],
