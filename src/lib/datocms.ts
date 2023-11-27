7c35484528e733b15904bc36c4d7f8bf3c373b8d
Jasper Moelker <jasper@voorhoede.nl>
2023-11-27 09:44:39 +0100

    feat(search): handle results without matching highlights

--- src/lib/datocms.ts
+++ src/lib/datocms.ts
@@ -51,6 +51,35 @@ export const datocmsRequest = <T>({ query, variables = {} }: DatocmsRequestType)
     });
 };
 
+const formatSearchResults = ({ query, results }: { query: string, results: RawSearchResult[] }) => {
+  return results.map((result) => {
+    const { title, body_excerpt, highlight, score, url } = result.attributes;
+    const defaultMatch = { 
+      matchingTerm: query,
+      markedText: body_excerpt.replace(/</g, '&lt;'), // escape HTML tags
+    };
+    const markedTextPattern = /\[h\](.+?)\[\/h\]/g;
+    const matches = (highlight.body || []).map(bodyText => ({
+      matchingTerm: markedTextPattern.exec(bodyText)?.[1] || query,
+      markedText: bodyText
+        .replace(/</g, '&lt;') // escape HTML tags
+        .replace(markedTextPattern, (_: string, text: string) => `<mark>${text}</mark>`),
+    }));
+    const { pathname } = new URL(url);
+    // use Text Fragment for deeplinking: https://developer.mozilla.org/en-US/docs/Web/Text_fragments
+    const textFragmentUrl = `${url}#:~:${ matches.map(({ matchingTerm }) => `text=${encodeURIComponent(matchingTerm)}`).join('&')}`;
+
+    return {
+      title: title.replace(new RegExp(`${titleSuffix}$`), '').trim(),
+      matches: matches.length ? matches : [defaultMatch],
+      score,
+      pathname,
+      textFragmentUrl,
+      url
+    };
+  });
+}
+
 export const datocmsSearch = async({ locale, query, fuzzy = true }: { locale: SiteLocale, query: string, fuzzy?: boolean }) => {
   const url = new URL('https://site-api.datocms.com/search-results');
   url.searchParams.set('locale', locale);
@@ -73,27 +102,7 @@ export const datocmsSearch = async({ locale, query, fuzzy = true }: { locale: Si
   }
 
   const { data, meta } = await response.json() as SearchResponse;
-
-  const results = data.map((result) => {
-    const { title, highlight, score, url } = result.attributes;
-    const markedTextPattern = /\[h\](.+?)\[\/h\]/g;
-    const matches = (highlight.body || []).map(bodyText => ({
-      matchingTerm: markedTextPattern.exec(bodyText)?.[1] || '',
-      markedText: bodyText.replace(markedTextPattern, (_: string, text: string) => `<mark>${text}</mark>`),
-    }));
-    const { pathname } = new URL(url);
-    // use Text Fragment for deeplinking: https://developer.mozilla.org/en-US/docs/Web/Text_fragments
-    const textFragmentUrl = `${url}#:~:${ matches.map(({ matchingTerm }) => `text=${encodeURIComponent(matchingTerm)}`).join('&')}`;
-
-    return {
-      title: title.replace(new RegExp(`${titleSuffix}$`), '').trim(),
-      matches,
-      score,
-      pathname,
-      textFragmentUrl,
-      url
-    };
-  });
+  const results = formatSearchResults({ query, results: data });
 
   return { meta, results };
 };
