8075ba9e96c519a99bf8575396cf77b04cb60b09
Jasper Moelker <jasper@voorhoede.nl>
2023-11-29 17:41:54 +0100

    feat(datocms): collection helper using pagination
    resolves Paginate Page Query #74

--- src/lib/datocms.ts
+++ src/lib/datocms.ts
@@ -1,4 +1,5 @@
 import 'astro/import-meta';
+import { parse } from 'graphql';
 import { print } from 'graphql/language/printer';
 import type { DocumentNode } from 'graphql';
 import { datocmsEnvironment } from '../../datocms-environment';
@@ -25,4 +26,57 @@ export const datocmsRequest = <T>({ query, variables = {} }: DatocmsRequestType)
       if (response.errors) throw Error(JSON.stringify(response, null, 4));
       return response.data;
     });
+};
+
+interface CollectionData<CollectionType> {
+  [key: string]: CollectionType[];
+}
+type CollectionMeta = {
+  count: number;
+};
+/**
+ * Returns all records from a DatoCMS collection (like 'Pages')
+ * with data for each record based on the provided fragment.
+ * 
+ * DatoCMS GraphQL API has a limit of 100 records per request.
+ * This function uses pagination to get all records.
+ * @see https://www.datocms.com/docs/content-delivery-api/pagination
+ */
+export const datocmsCollection = async <CollectionType>({ 
+  collection,
+  fragment
+}: { 
+  collection: string, 
+  fragment: string
+}) => {
+  const { meta } = await datocmsRequest({
+    query: parse(/* graphql */`
+      query ${collection}Meta {
+        meta: _all${collection}Meta { count }
+      }
+   `)
+  }) as { meta: CollectionMeta };
+
+  const recordsPerPage = 100; // DatoCMS GraphQL API has a limit of 100 records per request
+  const totalPages = Math.ceil(meta.count / recordsPerPage);
+  const records: CollectionType[] = [];
+
+  for (let page = 0; page < totalPages; page++) {
+    const data = await datocmsRequest({
+      query: parse(/* graphql */`
+        query All${collection} {
+          ${collection}: all${collection} (
+             first: ${recordsPerPage}, 
+             skip: ${page * recordsPerPage}
+          ) {
+            ${fragment}
+          }
+        }
+      `),
+    }) as CollectionData<CollectionType>;
+
+    records.push(...data[collection]);
+  }
+
+  return records;
 };
