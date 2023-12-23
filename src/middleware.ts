d3afaab6a7a754560920ccd64f2fcb355063dfda
Jasper Moelker <jasper@voorhoede.nl>
2023-12-23 02:13:41 +0100

    chore: lint on commit (#58)
    # Changes
    
    - Adds a git hook to lint all staged changes on commit to avoid linting
    issues making it into the remote repository (adapted from
    [tudelft-spacefinder](https://github.com/voorhoede/tudelft-spacefinder)).
    - Adds a GitHub Action to run linting on PR's (adapted from
    [tudelft-spacefinder](https://github.com/voorhoede/tudelft-spacefinder/blob/master/.github/workflows/node-ci.yml)).
    - Adds [`astro
    check`](https://docs.astro.build/en/reference/cli-reference/#astro-check),
    which finds issues that `eslint` with the `eslint-astro-parser` does not
    yet detect.
    - Fixes all current linting issues. (though I used 2x `ts-expect-error`
    and 1x `any` which I could use help with)
    
    # Associated issue
    
    N/A
    
    # How to test
    
    1. Checkout this branch
    2. Cause a linting issue
    3. Verify you have a linting issue by running `npm run lint`
    4. Stage your changes and commit them
    5. Verify linting is triggered and fixes are made when possible
    6. Undo your last commit :) (in case your linting issue was fixed
    automatically)
    
    # Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] I have made sure that my PR is easy to review (not too big,
    includes comments)
    - ~~I have added/updated tests to prove that my feature works (if not
    possible please explain why)~~
    - ~~I have made changes to the README and if the change affects the
    project setup (npm commands changed, new service added, environmental
    variable added)~~
    - ~~I have added a decision log entry if the change affects the
    architecture or changes a significant technology~~
    - [x] I have notified a reviewer
    
    <!-- Please strike through and check off all items that do not apply
    (rather than removing them) -->

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
 import { defineMiddleware, sequence } from 'astro/middleware';
 import { setLocale } from './lib/i18n';
+import type { SiteLocale } from '@lib/i18n.types';
 
 const i18n = defineMiddleware(async ({ params }, next) => {
   if (params.locale) {
-    setLocale(params.locale);
+    setLocale(params.locale as SiteLocale);
   }
   return next();
 });
