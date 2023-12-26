366e848b7d155209a144ab0902198a1c5c65d9bf
Jasper Moelker <jasper@voorhoede.nl>
2023-12-26 11:48:08 +0100

    feat: nested pages, url helpers & breadcrumbs
    to do:
    
    - [ ] use same ParentPage fragment everywhere
    - [ ] handle (parent) pages missing in some locales

---
+++ src/blocks/InternalLink/index.ts
@@ -0,0 +1,45 @@
+import type { PageRecord, SiteLocale } from '@lib/types/datocms';
+
+type AnyPage = PageRecord;
+
+export const getParentPages = (page: AnyPage): AnyPage[] => {
+  if (page.parentPage) {
+    return [
+      ...getParentPages(page.parentPage),
+      page.parentPage
+    ];
+  }
+  return [];
+};
+
+export const getParentSlugs = ({ page, locale }: { page: AnyPage, locale: SiteLocale }): string[] => {
+  if (page.parentPage) {
+    const slug = page.parentPage._allSlugLocales?.find(slug => slug.locale === locale)?.value;
+    return [
+      ...getParentSlugs({ page: page.parentPage, locale }), 
+      slug
+    ].filter(Boolean) as string[];
+  }
+  return [];
+};
+
+export const getPagePath = ({ page, locale }: { page: AnyPage, locale: SiteLocale }) => {
+  const slug = page._allSlugLocales?.find(slug => slug.locale === locale)?.value || page.slug;
+  const parentSlugs = getParentSlugs({ page, locale });
+  return [...parentSlugs, slug].join('/');
+};
+
+/**
+ * Returns the page slug from a URL path.
+ * 
+ * Returns page-1 in the following examples:
+ * - /page-1
+ * - /page-1/
+ * - /page-1/?foo=bar
+ * - /grand-parent/parent-slug/page-1
+ */
+export const getPageSlugFromPath = (path: URL['pathname']) => {
+  const url = new URL(path, 'https://www.example.com');
+  const slug = url.pathname.split('/').filter(Boolean).pop();
+  return slug;
+};
---
+++ src/components/Breadcrumbs/README.md
@@ -0,0 +1,14 @@
+# Breadcrumbs
+
+**Trail of links (hierarchial or chronological) to help users navigate and keep track of their location.**
+
+## Notes
+
+- Navigational element (`nav`) labeled `Breadcrumbs` (i18n), making it a landmark that's easy to locate.
+- Set of links is structured using an ordered list, to represent hierarchial / chronological order.
+- Current page link is marked as `aria-current="page"` for accessibility.
+
+## Relevant links
+
+- [ARIA Authoring Practices Guide: Breadcrumb](https://www.w3.org/WAI/ARIA/apg/patterns/breadcrumb/examples/breadcrumb/)
+- [component.gallery: Breadcrumbs](https://component.gallery/components/breadcrumbs/)
