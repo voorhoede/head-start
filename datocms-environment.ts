4d40fe8f2b9dd2eeec9a9dfeefdaea5259a3a7b7
Jasper Moelker <jasper@voorhoede.nl>
2024-01-20 14:33:04 +0100

    docs: decision to use datocms env file
    Resolves #106 (Move DatoCMS environment variables into .env file)
    
    Co-authored-by: Declan Rek <2034675+decrek@users.noreply.github.com>

--- datocms-environment.ts
+++ datocms-environment.ts
@@ -1,2 +1,7 @@
+/**
+ * DatoCMS environment configuration
+ * @see docs/getting-started.md on how to use this file
+ * @see docs/decision-log/2023-10-24-datocms-env-file.md on why file is preferred over env vars
+ */
 export const datocmsEnvironment = 'redirect-rules';
 export const datocmsBuildTriggerId = '30535';
