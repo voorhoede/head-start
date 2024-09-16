ca03e22b4c2744658f55531428dfcbcd57e01c04
Luuk <me@luuk.network>
2024-09-16 10:02:20 +0200

    Add more tests for embed block

--- src/blocks/ImageBlock/ImageBlock.test.ts
+++ src/blocks/ImageBlock/ImageBlock.test.ts
@@ -1,37 +1,61 @@
 import { renderToFragment } from '@lib/renderer';
-import type { ImageBlockFragment } from '@lib/types/datocms';
 import { describe, expect, test } from 'vitest';
-import ImageBlock from './ImageBlock.astro';
+import ImageBlock, { type Props as ImageBlockProps } from './ImageBlock.astro';
 
-const fragment = await renderToFragment<{ block: ImageBlockFragment }>(ImageBlock, {
-  props: {
-    block: {
-      id: '123',
-      image: {
-        url: 'https://example.com/',
-        alt: 'A test image',
-        title: 'A test image',
-      }
-    }
-  },
-});
+
 
 describe('ImageBlock', () => {
-  test('Component is rendered', () => {
-    expect(fragment).toBeDefined();
+  test('Component renders image', async () => {
+    const fragment = await renderToFragment<ImageBlockProps>(ImageBlock, {
+      props: {
+        block: {
+          id: '123',
+          image: {
+            url: 'https://example.com/',
+            alt: 'A test image',
+            title: 'A test image',
+            height: 150,
+            width: 150
+          }
+        }
+      },
+    });
+
+    expect(fragment.querySelector('figure')).toBeTruthy();
+    expect(fragment.querySelector('figcaption')).toBeTruthy();
+    expect(fragment.querySelector('figcaption')?.textContent).toBe('A test image');
+    expect(fragment.querySelector('img')).toBeTruthy();
+    expect(fragment.querySelector('img')?.getAttribute('loading')).toBe('lazy');
+    expect(fragment.querySelector('img')?.src).toBe('https://example.com/');
+    expect(fragment.querySelector('img')?.alt).toBe('A test image');
+    expect(fragment.querySelector('img')?.style.aspectRatio).toBe('150/150');
   });
 
-  test('Has all required elements', () => {
-    expect(fragment.querySelector('figure')).toBeDefined();
-    expect(fragment.querySelector('figcaption')).toBeDefined();
-    expect(fragment.querySelector('img')).toBeDefined();
-  });
+  test('Component renders responsive image', async () => {
+    const fragment = await renderToFragment<ImageBlockProps>(ImageBlock, {
+      props: {
+        block: {
+          id: '123',
+          image: {
+            url: 'https://example.com/',
+            alt: 'A test image',
+            title: 'A test image',
+            height: 150,
+            width: 150,
+            responsiveImage: {
+              aspectRatio: 1.9526315789473685,
+              base64: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoICAgLFQoWDRMVDhYWDhEPFg0VFxMeGBYVFhUoKy0jGh0oHRUWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLDg0OHBAQHDsoIhw7Ly8vLzs7OzsvLy81Ly8vLzUvNS87OzUvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIAA0AGAMBIgACEQEDEQH/xAAZAAABBQAAAAAAAAAAAAAAAAADAAIEBQf/xAAeEAABBAIDAQAAAAAAAAAAAAAEAAECAxIxESFBIv/EABQBAQAAAAAAAAAAAAAAAAAAAAP/xAAXEQEAAwAAAAAAAAAAAAAAAAACAAER/9oADAMBAAIRAxEAPwDWCCRY7k3KBEsTLaaWBVl6orgVM/TunIORiDkt6rhpN8yZJAECrx57SRWTVwbJ2f/Z'
+            }
+          }
+        }
+      },
+    });
 
-  test('Has all required props', () => {
-    const imgElement = fragment.querySelector('img')!;
-    expect(imgElement.src).toBe('https://example.com/');
-    expect(imgElement.alt).toBe('A test image');
-    expect(imgElement.height).not.toBeNull();
-    expect(imgElement.width).not.toBeNull();
+    expect(fragment.querySelector('figure')).toBeTruthy();
+    expect(fragment.querySelector('figcaption')).toBeTruthy();
+    expect(fragment.querySelector('figcaption')?.textContent).toBe('A test image');
+    expect(fragment.querySelector('img')).toBeTruthy();
+    expect(fragment.querySelector('img')?.style.aspectRatio).toBe('1.9526315789473685');
+    expect(fragment.querySelector('img')?.style.backgroundImage).toContain('base64');
   });
 });
