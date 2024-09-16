ea76aa84ca3fce3c1f9c8cb20a816f8c8c12e208
wesselsmit <wrsmit00@gmail.com>
2024-09-16 10:29:20 +0200

    fix: prevent vitest from hanging after tests until the timeout has been reached

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
-import { getViteConfig } from 'astro/config';
+import { defineConfig } from 'vitest/config';
 
-export default getViteConfig({
+export default defineConfig({
   test: {
     // Vitest configuration options
   },
