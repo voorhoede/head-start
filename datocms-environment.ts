a56998fbfd85eca314dfce5255aaa3b20dd9e506
Jurgen Beliën <jurgen@voorhoede.nl>
2024-10-15 13:14:31 +0200

    chore: set target environment

--- datocms-environment.ts
+++ datocms-environment.ts
@@ -3,5 +3,5 @@
  * @see docs/getting-started.md on how to use this file
  * @see docs/decision-log/2023-10-24-datocms-env-file.md on why file is preferred over env vars
  */
-export const datocmsEnvironment = 'page-model-names';
+export const datocmsEnvironment = '27-seed';
 export const datocmsBuildTriggerId = '30535';
