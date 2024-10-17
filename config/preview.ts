877514b4d3fadf2c71d9d9c92b41c7782035d0d1
decrek <declanrek@gmail.com>
2024-10-17 14:51:01 +0200

    fix(env): do not use getSecret but import env variables directly since getSecret is broken

--- config/preview.ts
+++ config/preview.ts
@@ -1,6 +1,6 @@
 import { execSync } from 'node:child_process';
 
-export const previewBranches = ['preview', 'feat/preview-mode', 'fix/function-size', 'fix/preview-mode-routing', 'preview-throws-500-due-to-Astro-bug-in-server-mode'];
+export const previewBranches = ['preview', 'feat/preview-mode', 'fix/function-size', 'fix/preview-mode-routing', 'preview-throws-500-due-to-Astro-bug-in-server-mode', 'fix/previews-not-working'];
 
 function getGitBranch() {
   const branch = execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf-8' }).trim();
