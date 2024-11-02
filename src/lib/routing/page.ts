388ad9a2728c34da3ca9bcefb62ba8b3529ea593
Jurgen Beliën <jurgen@voorhoede.nl>
2024-11-02 21:51:56 +0100

    refactor(routing): move path helper functions

--- src/lib/routing/page.ts
+++ src/lib/routing/page.ts
@@ -1,11 +1,14 @@
-import type { PageRecord, SiteLocale } from '@lib/types/datocms';
+import type { PageRouteFragment, SiteLocale } from '@lib/types/datocms';
 
-type AnyPage = PageRecord;
+export type PageRouteForPath =
+  & Pick<PageRouteFragment, '_allSlugLocales' | 'parentPage'>
+  & { parentPage?: PageRouteForPath | null };
+
 type MaybeSlug = string | undefined;
 
 const missingSlug = '-';
 
-export const getParentPages = (page: AnyPage): AnyPage[] => {
+export function getParentPages(page: PageRouteFragment): PageRouteFragment[] {
   if (page.parentPage) {
     return [
       ...getParentPages(page.parentPage),
@@ -13,12 +16,12 @@ export const getParentPages = (page: AnyPage): AnyPage[] => {
     ];
   }
   return [];
-};
+}
 
 /**
  * Returns a list of slugs for the parent pages of a page.
  * A (parent) page may not be available in all locales, so the list may contain undefined values.
- * 
+ *
  * Example return values:
  * - []                               (no parent pages)
  * - ['parent-slug']                  (parent page available in given locale)
@@ -26,11 +29,11 @@ export const getParentPages = (page: AnyPage): AnyPage[] => {
  * - ['grand-parent', 'parent-slug']  (grand parent and parent page available in given locale)
  * - ['grand-parent', undefined]      (grand parent page available, parent page not available in given locale)
  */
-export const getParentSlugs = ({ page, locale }: { page: AnyPage, locale: SiteLocale }): MaybeSlug[] => {
+export const getParentSlugs = ({ page, locale }: { page: PageRouteForPath, locale?: SiteLocale }): MaybeSlug[] => {
   if (page.parentPage) {
     const slug = page.parentPage._allSlugLocales?.find(slug => slug.locale === locale)?.value;
     return [
-      ...getParentSlugs({ page: page.parentPage, locale }), 
+      ...getParentSlugs({ page: page.parentPage, locale }),
       slug
     ];
   }
@@ -39,9 +42,9 @@ export const getParentSlugs = ({ page, locale }: { page: AnyPage, locale: SiteLo
 
 /**
  * Returns the URL path for a page based on its slug and the slugs of its parent pages.
- * A (parent) page may not be available in all locales, 
+ * A (parent) page may not be available in all locales,
  * in which case its slug is replaced with a dash.
- * 
+ *
  * Example return values:
  * - page-slug
  * - parent-slug/page-slug
@@ -50,7 +53,7 @@ export const getParentSlugs = ({ page, locale }: { page: AnyPage, locale: SiteLo
  * - -/-/page-slug                        (missing parent and grand parent in given locale)
  * - -                                    (missing page in given locale)
  */
-export const getPagePath = ({ page, locale }: { page: AnyPage, locale: SiteLocale }) => {
+export const getPagePath = ({ page, locale }: { page: PageRouteForPath, locale?: SiteLocale }) => {
   const slug = page._allSlugLocales?.find(slug => slug.locale === locale)?.value || missingSlug;
   const parentSlugs = getParentSlugs({ page, locale }).map(slug => slug || missingSlug);
   return [...parentSlugs, slug].join('/');
@@ -58,7 +61,7 @@ export const getPagePath = ({ page, locale }: { page: AnyPage, locale: SiteLocal
 
 /**
  * Returns the page slug from a URL path.
- * 
+ *
  * Returns page-1 in the following examples:
  * - /page-1
  * - /page-1/
