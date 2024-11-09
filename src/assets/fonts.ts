5a9cad9794dddc9205795cda6c70243befee81d6
Jasper Moelker <jasper@voorhoede.nl>
2024-11-09 15:24:48 +0100

    perf: inline css for custom font declarations

--- src/assets/fonts.ts
+++ src/assets/fonts.ts
@@ -1,9 +1,10 @@
-import '@fontsource/archivo/400.css';
-import '@fontsource/archivo/600.css';
-
+import archivo400css from '@fontsource/archivo/latin-400.css?raw'; // works in production
+import archivo600css from '@fontsource/archivo/latin-600.css?raw';
 import archivo400url from '@fontsource/archivo/files/archivo-latin-400-normal.woff2?url';
 import archivo600url from '@fontsource/archivo/files/archivo-latin-600-normal.woff2?url';
+import '@fontsource/archivo/latin-400.css'; // works local development
+import '@fontsource/archivo/latin-600.css';
 
-export const fontFamilyArchivo = 'Archivo, sans-serif';
-
+export const fontFamilyArchivo = 'Archivo, Times New Roman';
+export const fontCssDeclarations = [archivo400css, archivo600css];
 export const woff2urls = [archivo400url, archivo600url];
