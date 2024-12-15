d109920b8fc607c49723ebaadde8696cf93128d8
Jurgen Beliën <info@jurgenbelien.nl>
2024-12-15 15:52:13 +0100

    feat(Icon): expose support icon names

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,2 +1,2 @@
 // Icon component is often used, this makes it easier to import as `path/to/components/Icon`.
-export { default } from './Icon.astro';
+export { default, iconNames } from './Icon.astro';
