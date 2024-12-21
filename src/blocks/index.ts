0c8bf8c266185795658ce006d1d61f7a6f01868e
Jasper Moelker <jasper@voorhoede.nl>
2024-12-21 23:19:09 +0100

    fix: extract renderToString to resolve JSDOM conflicts
    However, this problem remains:
    ```
    [ERROR] [vite] x Build failed in 1.25s
    [commonjs--resolver] [plugin vite:resolve] Cannot bundle Node.js built-in "node:path" imported from "node_modules/astro/dist/core/config/schema.js". Consider disabling ssr.noExternal or remove the built-in dependency.
    ```

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,6 +1,15 @@
+import { experimental_AstroContainer as AstroContainer, type ContainerRenderOptions } from 'astro/container';
+import type { AstroComponentFactory } from 'astro/runtime/server/index.js';
 import type { AnyBlock } from './Blocks';
-import { renderToString } from '@lib/renderer';
 import Blocks from './Blocks.astro';
+
+export async function renderToString<Props>(
+  component: AstroComponentFactory,
+  options?: ContainerRenderOptions & { props?: Props }
+): Promise<string> {
+  const container = await AstroContainer.create();
+  return container.renderToString(component, options);
+}
 
 export const renderBlocks = async (blocks: AnyBlock[]) => {
   return await renderToString(Blocks, { props: { blocks } });
