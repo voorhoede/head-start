bc44d84247843a7efde5258bf0542d98e44935cb
Jasper Moelker <jasper@voorhoede.nl>
2024-12-21 19:52:34 +0100

    feat(preview): links from CMS related web pages
    This change contains 2 solutions:
    - an `/api/reroute/page` endpoint to find and redirect to the correct page url
    - a check in `pages/[locale]/[...path]/index.astro` that redirects to the canonical url if it doesn't match the current url

---
+++ config/datocms/migrations/1734806654_previewLinks.ts
@@ -0,0 +1,64 @@
+import { Client } from '@datocms/cli/lib/cma-client-node';
+
+export default async function (client: Client) {
+  console.log('Manage upload filters');
+
+  console.log('Install plugin "Model Deployment Links"');
+  await client.plugins.create({
+    id: 'MKba9NT5QBKZaeI4HcERwA',
+    package_name: 'datocms-plugin-model-deployment-links',
+  });
+  await client.plugins.update('MKba9NT5QBKZaeI4HcERwA', {
+    parameters: { datoApiToken: process.env.DATOCMS_PREVIEW_API_TOKEN },
+  });
+
+  console.log('Creating new fields/fieldsets');
+
+  console.log(
+    'Create JSON field "Preview" (`preview`) in model "\uD83D\uDCD1 Page" (`page`)'
+  );
+  await client.fields.create('LjXdkuCdQxCFT4hv8_ayew', {
+    id: 'XF2LuFVWSrmu7Lle8xlhTg',
+    label: 'Preview',
+    field_type: 'json',
+    api_key: 'preview',
+    localized: true,
+    appearance: {
+      addons: [],
+      editor: 'MKba9NT5QBKZaeI4HcERwA',
+      parameters: { urlPattern: '/{ locale }/{ slug }/' },
+    },
+  });
+
+  console.log(
+    'Create JSON field "Preview" (`preview`) in model "\uD83C\uDFE0 Home" (`home_page`)'
+  );
+  await client.fields.create('X_tZn3TxQY28ltSyjZUGHQ', {
+    id: 'YPXZOMoWRdKTHLUkN9ytfw',
+    label: 'Preview',
+    field_type: 'json',
+    api_key: 'preview',
+    localized: true,
+    appearance: {
+      addons: [],
+      editor: 'MKba9NT5QBKZaeI4HcERwA',
+      parameters: { urlPattern: '/{ locale }/' },
+    },
+  });
+
+  console.log(
+    'Create JSON field "Preview" (`preview`) in model "\uD83E\uDD37 Not found" (`not_found_page`)'
+  );
+  await client.fields.create('d_AvMVoMSqmNbMqx-NdqIw', {
+    id: 'O_DCfpaDSzq1MX4DaRlMpQ',
+    label: 'Preview',
+    field_type: 'json',
+    api_key: 'preview',
+    localized: true,
+    appearance: {
+      addons: [],
+      editor: 'MKba9NT5QBKZaeI4HcERwA',
+      parameters: { urlPattern: '/{ locale }/404' },
+    },
+  });
+}
