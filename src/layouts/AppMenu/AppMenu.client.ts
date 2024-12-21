f9e6f0583426f775dcfc2fd43ba27d6f0ed8945d
Jasper Moelker <jasper@voorhoede.nl>
2024-12-21 16:02:37 +0100

    feat(AppMenu): popover nested (mock) items (PoC)

--- src/layouts/AppMenu/AppMenu.client.ts
+++ src/layouts/AppMenu/AppMenu.client.ts
@@ -1,3 +1,6 @@
+import { computePosition } from '@floating-ui/dom';
+import type { Placement } from '@floating-ui/dom';
+
 class AppMenu extends HTMLElement {
   #menuButton: HTMLButtonElement;
   #dialog: HTMLDialogElement;
@@ -20,6 +23,20 @@ class AppMenu extends HTMLElement {
     this.#menuButton.focus();
   }
 
+  #popover (event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const button = target.closest('[popovertarget]') as HTMLElement;
+    const popoverId = target.getAttribute('popovertarget');
+    const popover = this.querySelector(`#${popoverId}`) as HTMLElement;
+    const placement = (popover?.getAttribute('data-placement') ?? 'bottom-start') as Placement;
+
+    if (!button || !popover) return;
+  
+    computePosition(button, popover, { placement }).then(({ x, y }) => {
+      Object.assign(popover.style, { left: `${x}px`, top: `${y}px` });
+    });
+  }
+
   #onResize() {
     this.classList.remove('is-compact');
     const isFitting = this.#menuList.scrollWidth <= this.#menuList.clientWidth;
@@ -29,6 +46,7 @@ class AppMenu extends HTMLElement {
   connectedCallback() {
     this.#menuButton.removeAttribute('hidden');
     this.#menuButton.addEventListener('click', this.open.bind(this));
+    this.#menuList.addEventListener('click', this.#popover.bind(this));
     this.#dialog.addEventListener('click', this.close.bind(this));
     this.#observer = new ResizeObserver(() => this.#onResize());
     this.#observer.observe(this);
