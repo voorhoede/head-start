c1db14fa0417b67fe12e1c2f39d6a0a5a8190850
Jasper Moelker <jasper@voorhoede.nl>
2025-01-04 05:58:50 +0100

    test(seo): include and cover robotsTxt
    note: moving to own branch as locally Astro build fails

--- src/lib/seo/index.ts
+++ src/lib/seo/index.ts
@@ -2,6 +2,7 @@ import type { Tag } from '@lib/datocms/types';
 import { getLocale } from '@lib/i18n';
 import type { SiteLocale } from '@lib/i18n/types';
 import { globalSeo } from '@lib/site.json';
+import aiRobotsTxt from './ai.robots.txt';
 
 export type PageUrl = {
   locale: SiteLocale,
@@ -32,3 +33,19 @@ export const titleTag = (title: string): Tag => ({
   tag: 'title',
   content: `${title} ${titleSuffix()}`,
 });
+
+export type RobotsTxtProps = {
+  allowAiBots: boolean,
+  allowAll: boolean,
+  siteUrl: string,
+}
+
+export const robotsTxt = ({ allowAiBots, allowAll, siteUrl }: RobotsTxtProps) => `
+${allowAiBots ? '' : aiRobotsTxt}
+
+User-agent: *
+${allowAll ? 'Allow: /' : 'Disallow: /'}
+
+Sitemap: ${siteUrl}/sitemap-index.xml
+
+`.trim();
--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,32 +1,17 @@
 // robots.txt contains dynamic content, which can be determined at build time.
 // so we use this API route to prerender the robots.txt file.
 import type { APIRoute } from 'astro';
-import aiRobotsTxt from '@lib/seo/ai.robots.txt';
 import { datocmsRequest } from '@lib/datocms';
 import type { RobotsTxtQuery } from '@lib/datocms/types';
+import { robotsTxt } from '@lib/seo';
 import query from './_robots.query.graphql';
 
 export const prerender = true;
 
-interface Props {
-  allowAiBots: boolean;
-  allowAll: boolean;
-  siteUrl: string;
-}
-const robotsTxt = ({ allowAiBots, allowAll, siteUrl }: Props) => `
-${allowAiBots ? '' : aiRobotsTxt}
-
-User-agent: *
-${allowAll ? 'Allow: /' : 'Disallow: /'}
-
-Sitemap: ${siteUrl}/sitemap-index.xml
-
-`.trim();
-
 export const GET: APIRoute = async (context) => {
   const { /*app, */ site } = await datocmsRequest<RobotsTxtQuery>({ query });
-  const allowAiBots = false; /* app?.allowAiBots; */
   const allowAll = !site.noIndex && !context.locals.isPreview;
+  const allowAiBots = allowAll && false; /* app?.allowAiBots; */
 
   return new Response(robotsTxt({
     allowAiBots,
