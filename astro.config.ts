30216d59dd28e14c739fd351f1fdb10442bdd16c
Jasper Moelker <jasper@voorhoede.nl>
2025-01-05 12:04:23 +0100

    chore: "analyze" script
    Adds npm script to analyze and visualise both client & server bundles.

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,12 @@ import { defineConfig, envField, passthroughImageService } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import graphql from '@rollup/plugin-graphql';
 import sitemap from '@astrojs/sitemap';
+import Sonda from 'sonda/astro';
 import type { PluginOption } from 'vite';
 import { isPreview } from './config/preview';
 import pkg from './package.json';
 
+const isAnalyseMode = process.env.ANALYZE === 'true';
 const productionUrl = `https://${ pkg.name }.pages.dev`; // overwrite if you have a custom domain
 const localhostPort = 4323; // 4323 is "head" in T9
 export const siteUrl = process.env.CF_PAGES
@@ -50,11 +52,21 @@ export default defineConfig({
     // @see https://docs.astro.build/en/guides/images/#configure-no-op-passthrough-service
     service: passthroughImageService()
   },
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(),
+    Sonda({
+      enabled: isAnalyseMode,
+      filename: 'reports/sonda-report-[env].html',
+      server: true,
+    }),
+  ],
   output: isPreview ? 'server' : 'static',
   server: { port: localhostPort },
   site: siteUrl,
   vite: {
+    build: {
+      sourcemap: isAnalyseMode,
+    },
     plugins: [
       graphql() as PluginOption,
     ],
