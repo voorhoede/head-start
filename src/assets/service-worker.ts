d5f270594f9a4801843e5a3086eae9a53be8c2d3
decrek <declanrek@gmail.com>
2025-01-10 15:14:16 +0100

    refactor: less ts magic

--- src/assets/service-worker.ts
+++ src/assets/service-worker.ts
@@ -1,31 +1,32 @@
 /// <reference lib="webworker" />
+declare const self: ServiceWorkerGlobalScope;
+export default null;
+
 import { registerRoute } from 'workbox-routing';
 import { NetworkFirst } from 'workbox-strategies';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 import { ExpirationPlugin } from 'workbox-expiration';
 
-((self: ServiceWorkerGlobalScope) => {
-  self.addEventListener('install', () => {
-    self.skipWaiting();
-  });
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
   
-  self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
-  });
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
 
-  registerRoute(
-    ({ request }) => request.mode === 'navigate',
-    new NetworkFirst({
-      networkTimeoutSeconds: 3,
-      cacheName: 'pages',
-      plugins: [
-        new CacheableResponsePlugin({
-          statuses: [0, 200],
-        }),
-        new ExpirationPlugin({
-          maxEntries: 30,
-        }),
-      ],
-    })
-  );
-})(self as unknown as ServiceWorkerGlobalScope);
+registerRoute(
+  ({ request }) => request.mode === 'navigate',
+  new NetworkFirst({
+    networkTimeoutSeconds: 3,
+    cacheName: 'pages',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
