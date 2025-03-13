7bd84822f5b70b465e621965cebe40b9d4296974
Jurgen Beliën <info@jurgenbelien.nl>
2025-03-13 10:22:17 +0100

    feat(collections): map slug to collection item id, add locale prefix

--- src/lib/collections/pages.ts
+++ src/lib/collections/pages.ts
@@ -1,17 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 import { PageRoute as fragment, type PageRouteFragment } from '@lib/datocms/types';
 import { datocmsCollection } from '@lib/datocms';
+import { mapSlugsToIds, type Mapped } from './lib/slug';
 
 const name = 'Pages' as const;
 
 const loader = () => datocmsCollection<PageRouteFragment>({
   collection: name,
   fragment,
-});
+}).then(mapSlugsToIds);
 
 const collection = defineCollection({
   loader: loader,
-  schema: z.custom<PageRouteFragment>(),
+  schema: z.custom<Mapped<PageRouteFragment>>(),
 });
 
 export default {
