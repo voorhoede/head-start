1ce8ab1feace7c126147426abf5f1c7a02e51197
Jurgen Beliën <info@jurgenbelien.nl>
2025-03-17 13:28:09 +0100

    style(blocks): loosen AnyBlock type by allowing any typename

--- src/blocks/Blocks.d.ts
+++ src/blocks/Blocks.d.ts
@@ -10,13 +10,16 @@ import type {
   VideoEmbedBlockFragment,
 } from '@lib/datocms/types';
 
-export type AnyBlock =
-  | ActionBlockFragment
-  | EmbedBlockFragment
-  | ImageBlockFragment
-  | PagePartialBlockFragment
-  | TableBlockFragment
-  | TextBlockFragment
-  | TextImageBlockFragment
-  | VideoBlockFragment
-  | VideoEmbedBlockFragment;
+export type AnyBlock = Omit<
+  ( // Add any new Block types here.
+    | ActionBlockFragment
+    | EmbedBlockFragment
+    | ImageBlockFragment
+    | PagePartialBlockFragment
+    | TableBlockFragment
+    | TextBlockFragment
+    | TextImageBlockFragment
+    | VideoBlockFragment
+    | VideoEmbedBlockFragment
+  ), '__typename' // Allow for any __typename so that missing blocks can be reported on.
+> & { __typename: string };
