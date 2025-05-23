11edcde158c14f0c9e0bbed87a0413bded68cdee
MarleenEliza <marleen@voorhoede.nl>
2025-05-23 14:15:32 +0200

    add inquirer

--- scripts/cms-apply-migration.ts
+++ scripts/cms-apply-migration.ts
@@ -1,4 +1,8 @@
 import { execCommand } from './lib/exec-command';
 import { datocmsEnvironment } from '../datocms-environment';
 
-execCommand(`npx datocms migrations:run --destination=${datocmsEnvironment} --fast-fork`);
+const confirmationMessage = `apply all migrations in chronological order to the environment '${datocmsEnvironment}'`;
+execCommand(
+  `npx datocms migrations:run --destination=${datocmsEnvironment} --fast-fork`,
+  confirmationMessage,
+);
--- scripts/lib/exec-command.ts
+++ scripts/lib/exec-command.ts
@@ -1,12 +1,34 @@
 import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
+import inquirer from 'inquirer';
 
-export const execCommand = async (command: string) => {
-  const execAsync = promisify(exec);
-  try {
-    const { stdout } = await execAsync(command);
-    console.log(stdout);
-  } catch (error) {
-    console.error(error);
+// npx datocms environments:fork grouping-block test-migration --fast
+
+export const execCommand = async (
+  command: string,
+  confirmationMessage: string,
+) => {
+  const isConfirmed: { confirm: boolean } = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'confirm',
+      message: `⚠️  ${confirmationMessage}\n\nAre you sure you want to continue?`,
+      default: false,
+      theme: {
+        prefix: 'This command will: \n',
+      },
+    },
+  ]);
+  if (isConfirmed.confirm) {
+    const execAsync = promisify(exec);
+
+    try {
+      const { stdout } = await execAsync(command);
+      console.log(stdout);
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.log('execution cancelled');
   }
 };
