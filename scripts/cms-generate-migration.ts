8b24c0c7ce958ff57607034906adc28cb7fce19d
MarleenEliza <marleen@voorhoede.nl>
2025-05-23 14:42:02 +0200

    add inquirer

--- scripts/cms-generate-migration.ts
+++ scripts/cms-generate-migration.ts
@@ -1,4 +1,12 @@
 import { execCommand } from './lib/exec-command';
 import { datocmsEnvironment } from '../datocms-environment';
+import { stripIndents } from 'proper-tags';
 
-execCommand(`npx datocms migrations:new --autogenerate=${datocmsEnvironment} ${datocmsEnvironment}`);
+const confirmationMessage = stripIndents`
+  Create a new migration in 'config/datocms/migrations' based on the 
+  differences between the primary environment and environment '${datocmsEnvironment}'`;
+
+execCommand(
+  `npx datocms migrations:new --autogenerate=${datocmsEnvironment} ${datocmsEnvironment}`,
+  confirmationMessage,
+);
