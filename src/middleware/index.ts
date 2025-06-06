cba55dfe10d3e44c1e854b6183e931a5f0de8595
Jasper Moelker <jasper@voorhoede.nl>
2025-06-06 13:56:38 +0200

    feat: runtime security headers
    #72 added basic security headers that are only applied to static files. This change adds the same security headers to runtime responses using Astro middleware.

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -5,11 +5,13 @@ import { i18n } from './i18n';
 import { preview } from './preview';
 import { proxyFiles } from './proxy-files';
 import { redirects } from './redirects';
+import { securityheaders } from './security-headers';
 
 export const onRequest = sequence(
   datocms,
   i18n,
   preview,
   proxyFiles,
-  redirects
+  redirects,
+  securityheaders,
 );
