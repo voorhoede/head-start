54ce7d9b5a0864077c8f2515cc39e12fd2cb26cf
MarleenEliza <marleen@voorhoede.nl>
2025-06-26 11:47:52 +0200

    add docs for new datocms prompts

---
+++ scripts/cms-helper.ts
@@ -0,0 +1,97 @@
+import { select } from '@inquirer/prompts';
+
+async function run() {
+  const answer = await select({
+    message: 'What do you want to do?',
+    choices: [
+      {
+        name: '🆕 start development in a new sandbox environment',
+        value: 'env:create',
+        description:
+          'Create a new sandbox environment. You can set the name of the sandbox environment and wheter you would like to run all new migration files or not.',
+      },
+      {
+        name: '🔁 sync my sandbox environment with migration files',
+        value: 'env:sync',
+        description:
+          'Sync your sandbox environment will run all new migrations in-place. You can choose the name of the target sandbox environment.',
+      },
+      {
+        name: '🗑️ get rid of old sandbox environment',
+        value: 'env:destroy',
+        description:
+          'Destroy a sandbox environment. You can choose the name of the target sandbox environment. This will delete all content in the environment.',
+      },
+      {
+        name: '🧪 test run my migrations in a new env',
+        value: 'env:create',
+        description:
+          'Create a new sandbox environment. Ensure that you select YES when asked to run all new migration files in the new environment.',
+      },
+      {
+        name: '🔄 generate migration files',
+        value: 'migration:generate',
+        description:
+          'Generate migration files. This will create a new migration file in the `migrations` directory that has all schema changes between the primary environment and the target sandbox environment.',
+      },
+      {
+        name: '🚀 promote sandbox environment to production',
+        value: 'env:promote',
+        description:
+          'Promote a sandbox environment to primary. You will have the option to delete the old primary environment upon success.',
+      },
+    ],
+  });
+
+  console.log(answer);
+
+
+  switch (answer) {
+  case 'env:create':  
+    // TODO: improve creation script
+    // - ask for name of the environment
+    // - ask if you want to run all new migration files
+    // - upon succes, update local environment variable in datocms-environment.ts
+    // - upon success, give feedback
+    // await cmsCreateEnvironment();
+    break;
+  case 'env:sync':
+    // TODO: add sync script instead of migrations:run
+    // - ask for verification to run migrations in-place
+    // - ask for name of the environment
+    // - upon success, update local environment variable in datocms-environment.ts
+    // - upon success, give feedback
+    // await cmsSyncEnvironment();
+    break;
+  case 'env:destroy':
+    // TODO: improve destroy script
+    // - ask for verification to destroy the environment, Action is irreversible
+    // - ask for name of the environment
+    // - upon succes, give feedback
+    // - upon success, ask if user wants to rename local environment variable in datocms-environment.ts
+    // - upon success, update local environment variable in datocms-environment.ts
+    // await cmsDestroyEnvironment();
+    break;
+  case 'migration:generate':
+    // TODO: improve migration generation script
+    // - ask for name of the environment
+    // - upon success, give feedback
+    // await cmsGenerateMigration();
+    break;
+  case 'env:promote':
+    // TODO: improve promote script
+    // - ask for name of the environment
+    // - ask for verification to promote the environment
+    // - maintenance on primary environment
+    // - promote sandbox environment to primary
+    // - maintenance off primary environment
+    // - upon success, update local environment variable in datocms-environment.ts
+    // - upon success, ask if user wants to delete the old primary environment
+    // - upon success, delete the old primary environment
+    // - upon success, give feedback
+    // await cmsPromoteEnvironment();
+    break;
+  }
+}
+
+run();
--- scripts/cms-sync-environment.ts
+++ scripts/cms-sync-environment.ts
@@ -2,7 +2,11 @@ import { execCommand } from './lib/exec-command';
 import { datocmsEnvironment } from '../datocms-environment';
 
 const confirmationMessage = `Apply all migrations in chronological order to the environment '${datocmsEnvironment}'`;
-execCommand(
-  `npx datocms migrations:run --destination=${datocmsEnvironment} --fast-fork`,
-  confirmationMessage,
-);
+export default async function run() {
+  execCommand(
+    `npx datocms migrations:run --source=${datocmsEnvironment} --in-place`,
+    confirmationMessage,
+  );
+}
+
+run();
