c57ab750264380488190e3503fdf91f1bf8dce1b
Luuk <luuk@voorhoede.nl>
2025-07-18 12:06:34 +0200

    Simplify ESLint config and use typescript-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,12 @@
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
+import ts from 'typescript-eslint';
 import astro from 'eslint-plugin-astro';
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
 
 export default defineConfig([
   js.configs.recommended,
+  ts.configs.base,
   astro.configs['flat/recommended'],
   {
     languageOptions: {
@@ -20,41 +14,20 @@ export default defineConfig([
         ...globals.node,
         ...globals.browser,
       },
-      
       ecmaVersion: 'latest',
-      sourceType: 'module',
-      parserOptions: {},
+      sourceType: 'module'
     },
 
     rules: {
       indent: ['warn', 2],
       quotes: ['warn', 'single'],
-      'object-curly-spacing': ['warn', 'always'],
-      'no-unused-vars': ['warn'],
       semi: ['warn', 'always'],
-    },
-  },
-  {
-    files: ['**/*.ts'],
-
-    languageOptions: {
-      parser: tsParser,
-    },
-
-    extends: compat.extends('plugin:@typescript-eslint/recommended'),
-
-    rules: {
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-        },
-      ],
-
+      'object-curly-spacing': ['warn', 'always'],
+      'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_' }],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' }],
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/triple-slash-reference': 'off',
-    },
+    }
   },
   {
     ignores: [
