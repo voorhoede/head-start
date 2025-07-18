1f62ef207f6a7b43cf596227b8bfa2ac1e513c11
Luuk <luuk@voorhoede.nl>
2025-07-18 15:53:21 +0200

    chore: flat eslint config (#275)
    # Changes
    
    - Upgrade eslint config
    - Fix errors caused by new config
    
    # Associated issue
    
    N.A.
    
    # How to test
    
    1. Check if lint action is passing
    2. Check code for changes
    
    # Checklist
    
    - [x] I have performed a self-review of my own code
    - [x] I have made sure that my PR is easy to review (not too big,
    includes comments)
    - [ ] I have made updated relevant documentation files (in project
    README, docs/, etc)
    - [ ] I have added a decision log entry if the change affects the
    architecture or changes a significant technology
    - [x] I have notified a reviewer
    
    <!-- Please strike through and check off all items that do not apply
    (rather than removing them) -->

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import astro from 'eslint-plugin-astro';
+
+export default defineConfig([
+  js.configs.recommended,
+  ts.configs.recommended,
+  astro.configs['flat/recommended'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    rules: {
+      indent: ['warn', 2],
+      quotes: ['warn', 'single'],
+      semi: ['warn', 'always'],
+      'object-curly-spacing': ['warn', 'always'],
+      'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_' }],
+    }
+  },
+  {
+    files: ['**/*.ts'],
+    rules: {
+      'no-unused-vars': ['off'],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' }],
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/triple-slash-reference': 'off',
+    }
+  },
+  {
+    ignores: [
+      '**/*.astro', 
+      'dist/*', 
+      'functions/*', 
+      'src/lib/datocms/types.ts',
+      '!**/.graphqlrc.ts'
+    ]
+  }
+]);
