81608349c295580caf9083248b0e4b356c769612
Luuk <luuk@voorhoede.nl>
2025-07-18 10:57:01 +0200

    Use flat config style for ESLint and astro

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,73 +2,86 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import parser from 'astro-eslint-parser';
 import tsParser from '@typescript-eslint/parser';
-import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import astro from 'eslint-plugin-astro';
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
+  allConfig: js.configs.all,
 });
 
-export default defineConfig([{
-  languageOptions: {
-    globals: {
-      ...globals.node,
-      ...globals.browser,
+export default defineConfig([
+  js.configs.recommended,
+  astro.configs['flat/recommended'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {},
     },
 
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    parserOptions: {},
-  },
-
-  extends: compat.extends('eslint:recommended', 'plugin:astro/recommended'),
-
-  rules: {
-    indent: ['warn', 2],
-    quotes: ['warn', 'single'],
-    'object-curly-spacing': ['warn', 'always'],
-    'no-unused-vars': ['warn', {
-      varsIgnorePattern: '^Props$',
-    }],
-    semi: ['warn', 'always'],
-  },
-}, globalIgnores(['dist/*', 'functions/*', 'node_modules/*']), {
-  files: ['**/*.astro'],
-
-  languageOptions: {
-    parser: parser,
-
-    parserOptions: {
-      parser: '@typescript-eslint/parser',
-      extraFileExtensions: ['.astro'],
+    rules: {
+      indent: ['warn', 2],
+      quotes: ['warn', 'single'],
+      'object-curly-spacing': ['warn', 'always'],
+      'no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^Props$',
+        },
+      ],
+      semi: ['warn', 'always'],
     },
   },
+  globalIgnores(['dist/*', 'functions/*', 'node_modules/*']),
+  {
+    files: ['**/*.astro'],
 
-  rules: {},
-}, {
-  files: ['**/*.ts'],
+    languageOptions: {
+      parser: parser,
 
-  languageOptions: {
-    parser: tsParser,
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+    },
+
+    rules: {},
   },
+  {
+    files: ['**/*.ts'],
 
-  extends: compat.extends('plugin:@typescript-eslint/recommended'),
+    languageOptions: {
+      parser: tsParser,
+    },
 
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['error', {
-      argsIgnorePattern: '^_',
-      destructuredArrayIgnorePattern: '^_',
-    }],
+    extends: compat.extends('plugin:@typescript-eslint/recommended'),
 
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/triple-slash-reference': 'off',
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
+
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/triple-slash-reference': 'off',
+    },
   },
-}, globalIgnores([
-  '**/dist',
-  '**/node_modules',
-  '**.astro/**',
-  'src/lib/datocms/types.ts',
-  '!**/.graphqlrc.ts',
-])]);
+  globalIgnores([
+    '**/dist',
+    '**/node_modules',
+    '**.astro/**',
+    'src/lib/datocms/types.ts',
+    '!**/.graphqlrc.ts',
+  ]),
+]);
