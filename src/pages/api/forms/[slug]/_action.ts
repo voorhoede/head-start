79258379c1cffdc8c08645463623e29fbbf5807b
Bazottie <basdegreeuw@voorhoede.nl>
2025-09-12 17:12:37 +0200

    Update action loading and component response structure

--- src/pages/api/forms/[slug]/_action.ts
+++
@@ -1,8 +0,0 @@
-import type { parseFormSubmission } from '@lib/forms/parse.ts';
-import type { CollectionEntry } from '@lib/content';
-
-export default async function handleFormSubmission<T extends CollectionEntry<'Forms'>['data']>(
-  result: Awaited<ReturnType<typeof parseFormSubmission<T>>>
-) {
-  console.log(result);
-}
