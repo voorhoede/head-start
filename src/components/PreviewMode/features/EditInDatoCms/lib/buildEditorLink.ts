2b8103c55fd5fdc26ac973fa6d586c03320571c8
Ani Reyna <anareymej@gmail.com>
2026-01-26 15:41:11 +0100

    remove EditInDatoCms component and  link generation logic

--- src/components/PreviewMode/features/EditInDatoCms/lib/buildEditorLink.ts
+++ src/components/PreviewMode/features/EditInDatoCms/lib/buildEditorLink.ts
@@ -13,27 +13,3 @@ export function getProjectName(): string | null {
   return domain.split('.')[0] || null;
 }
 
-export function getDatoCmsEditorLink(
-  recordId: string | null,
-  itemTypeId: string | null,
-  projectName: string | null,
-  environment: string,
-): string | null {
-  if (!recordId || !itemTypeId || !projectName || !environment) return null;
-
-  const baseUrl = `https://${projectName}.admin.datocms.com/environments/${environment}/editor`;
-
-  return `${baseUrl}/item_types/${itemTypeId}/items/${recordId}/edit`;
-}
-
-export function buildEditorLink(
-  recordId: string | null,
-  itemTypeId: string | null,
-  environment: string,
-): string | null {
-  const projectName = getProjectName();
-  if (!projectName) return null;
-
-  return getDatoCmsEditorLink(recordId, itemTypeId, projectName, environment);
-}
-
