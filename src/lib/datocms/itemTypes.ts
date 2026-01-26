3633a5f1ed3ee21582b99573ba8aba670bc778e0
Ani Reyna <anareymej@gmail.com>
2026-01-26 15:44:25 +0100

    remove unused itemTypes.ts file

--- src/lib/datocms/itemTypes.ts
+++
@@ -1,6 +0,0 @@
-// import itemTypesJson from './itemTypes.json';
-
-// export function getItemTypeId(typename: string | undefined): string | null {
-//   if (!typename) return null;
-//   return itemTypesJson[typename] ?? null;
-// }
