d1dc0065fd941e11bb1e658fcb74160a99ae2983
milailijevic <mila.ilijevic@gmail.com>
2026-02-05 18:44:19 +0100

    migration file

--- datocms-environment.ts
+++ datocms-environment.ts
@@ -2,5 +2,5 @@
  * @see docs/getting-started.md on how to use this file
  * @see docs/decision-log/2023-10-24-datocms-env-file.md on why file is preferred over env vars
  */
-export const datocmsEnvironment = 'feat-video-block-hint';
+export const datocmsEnvironment = 'feat-nested-menu';
 export const datocmsBuildTriggerId = '34548';
