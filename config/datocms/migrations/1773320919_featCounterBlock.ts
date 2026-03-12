5b245d9a24f5cf9f12ce06c0778aefeabd89603b
milailijevic <mila.ilijevic@gmail.com>
2026-03-12 14:10:52 +0100

    new migration file 1773320919_featCounterBlock.ts

--- config/datocms/migrations/1773320919_featCounterBlock.ts
+++ config/datocms/migrations/1773320919_featCounterBlock.ts
@@ -40,4 +40,35 @@ export default async function (client: Client) {
     },
     default_value: null,
   });
+
+  console.log('Update existing fields/fieldsets');
+
+  console.log(
+    'Update Structured text field "Text" (`text`) in block model "\uD83D\uDCDD Text Block" (`text_block`)',
+  );
+  await client.fields.update('NtVXfZ6gTL2sKNffNeUf5Q', {
+    validators: {
+      required: {},
+      structured_text_blocks: {
+        item_types: [
+          'F60ZY1wFSW2qbvh99poj3g',
+          'QYfZyBzIRWKxA1MinIR0aQ',
+          'ZdBokLsWRgKKjHrKeJzdpw',
+          'gezG9nO7SfaiWcWnp-HNqw',
+          '0SxYNS2CR1it_5LHYWuEQg',
+        ],
+      },
+      structured_text_inline_blocks: { item_types: ['Yj11fFgoThKqLyKcqIg2Gg'] },
+      structured_text_links: {
+        on_publish_with_unpublished_references_strategy: 'fail',
+        on_reference_unpublish_strategy: 'delete_references',
+        on_reference_delete_strategy: 'delete_references',
+        item_types: [
+          'GjWw8t-hTFaYYWyc53FeIg',
+          'LjXdkuCdQxCFT4hv8_ayew',
+          'X_tZn3TxQY28ltSyjZUGHQ',
+        ],
+      },
+    },
+  });
 }
