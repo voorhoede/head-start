92893f47dc707d7ecac362a991b6ada4d019f4f7
Jop Molenaar <jopmolenaar@icloud.com>
2026-04-17 10:21:57 +0200

    fix(): fixed build problems fontloader

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,7 @@ import pkg from './package.json';
 import { isPreview } from './config/preview';
 import { output } from './config/output';
 import serviceWorker from './config/astro/service-worker-integration.ts';
-import { fontsConfig } from './src/assets/fonts';
+import { fontConfig } from './src/assets/fonts';
 
 const isAnalyseMode = process.env.ANALYZE === 'true';
 const productionUrl = `https://${pkg.name}.pages.dev`; // overwrite if you have a custom domain
@@ -76,6 +76,6 @@ export default defineConfig({
     }
   },
   experimental: {
-    fonts: fontsConfig,
+    fonts: fontConfig, // @note this can be moved out of experimental when we updated astro to v6.0
   }
 });
