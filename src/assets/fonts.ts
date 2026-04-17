2ab70c567618412122021e13700431dbeff90674
Jop Molenaar <jopmolenaar@icloud.com>
2026-04-17 10:28:18 +0200

    chore(): removes comment

--- src/assets/fonts.ts
+++ src/assets/fonts.ts
@@ -1,9 +1,6 @@
 // // example use of custom fonts. Modify fontsConfig with your own fonts.
 // Based on https://docs.astro.build/en/guides/fonts/
 
-// @note still experimental in in this version of Astro,
-// its stable in astro v5.7 and gets improvements in v6.0
-
 import { fontProviders, type FontFamily } from 'astro/config';
 
 export const fontConfig: FontFamily[] = [
