9d07d0dd5029bca92274c10e49f52dc77fc56983
Jop Molenaar <jopmolenaar@icloud.com>
2026-07-10 09:52:58 +0200

    fix(): renaming variable, use ai prompt from data.app in order to have the right language

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -15,7 +15,7 @@ export async function getApp(): Promise<App> {
   return cache;
 }
 
-export const isAllowAiBots = async (isPreview = false) => {
+export const isAiBotsAllowed = async (isPreview = false) => {
   const app = await getApp();
   return !app.noIndex && !isPreview && Boolean(app.allowAiBots);
 };
